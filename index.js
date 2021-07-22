dab5ef4767e44157c13ab7092c3718e681145927
jmhar <jmhar@protonmail.com>
2021-07-22 14:34:12 +1000

    practice fetch

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 }
+
+// function fetchBooks() {
+//   fetch('https://anapioficeandfire.com/api/books')
+//     .then(resp => resp.json())
+//     .then(json => console.log(json[4].characters[1030]));
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
